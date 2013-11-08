75c67ee9b93803d650ae453b7b5ec2061cd13502
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2013-11-08 19:26:01 +0200

    added "south" for schema and data migrations

--- sopler/settings.py
+++ sopler/settings.py
@@ -159,6 +159,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.persona',
     #'allauth.socialaccount.providers.openid',
     'core',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
