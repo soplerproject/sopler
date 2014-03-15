fe576f8b459dee4c2865bee001e3cef78a3e9c0a
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2014-03-15 05:58:58 -0400

    Added Facebook pagination support.

--- social_friends_finder/backends/facebook_backend.py
+++ social_friends_finder/backends/facebook_backend.py
@@ -1,43 +1,62 @@
 from social_friends_finder.backends import BaseFriendsProvider
 from social_friends_finder.utils import setting
+import facebook
+import urlparse
 
 if setting("SOCIAL_FRIENDS_USING_ALLAUTH", False):
     USING_ALLAUTH = True
     from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
+    
 else:
     USING_ALLAUTH = False
-    #from social_auth.backends.facebook import FacebookBackend
 
-import facebook
     
 class FacebookFriendsProvider(BaseFriendsProvider):
 
-    def fetch_friends(self, user):
+    def fetch_friends(self, user, paginate=False):
         """
         Fethces friends from facebook.
         Returns:
             collection of friend objects fetched from facebook
         """
+        
         if USING_ALLAUTH:
 	  social_app = SocialApp.objects.get_current('facebook')
 	  oauth_token = SocialToken.objects.get(account=user, app=social_app).token
             
         else:
 	  pass
-	  #social_auth_backend = FacebookBackend()
-	  #tokens = social_auth_backend.tokens(user)
-	  #oauth_token = tokens['access_token']
-
+	
+	# Get the access_token
         graph = facebook.GraphAPI(oauth_token)
-        return graph.get_connections("me", "friends")
+	friends = graph.get_connections("me", "friends")
 
-    def fetch_friend_ids(self, user):
+        if paginate:
+            total_friends = friends.copy()
+            total_friends.pop('paging')
+            
+            while 'paging' in friends and 'next' in friends['paging'] and friends['paging']['next']:
+                next_url = friends['paging']['next']
+                next_url_parsed = urlparse.urlparse(next_url)
+                query_data = urlparse.parse_qs(next_url_parsed.query)
+                query_data.pop('access_token')
+                for k, v in query_data.items():
+                    query_data[k] = v[0]
+                    
+                friends = graph.get_connections("me", "friends", **query_data)
+                total_friends['data'] = sum([total_friends['data'], friends['data']], [])
+        else:
+            total_friends = friends
+
+        return total_friends
+
+    def fetch_friend_ids(self, user, **kwargs):
         """
         Fethces friend id's from facebook
         Return:
             collection of friend ids
         """
-        friends = self.fetch_friends(user)
+        friends = self.fetch_friends(user, **kwargs)
         friend_ids = []
         for friend in friends['data']:
             friend_ids.append(friend['id'])
